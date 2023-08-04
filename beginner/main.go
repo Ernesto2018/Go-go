cbc23e7239b7aa95a6b12f493cf39fccd8b97b6d
Ondieki Ernest <41272301+Ernesto2018@users.noreply.github.com>
2023-08-04 22:11:29 +0300

    functions
    functions, if...else, structs

--- beginner/main.go
+++ beginner/main.go
@@ -5,50 +5,122 @@ import (
 	"strings"
 )
 
-func main() {
+const totalTickets = 3
 
-	var conferenceName = "Go Conference" // type string
-	const conferenceTickets = 50         // constant value that never changes
-	var remainingTickets uint = 50       // unsigned int (positive integers only)
-	var bookings []string                // slice type
+var availableTickets uint = 3
 
-	fmt.Printf("Welcome to %v booking app\n", conferenceName)
-	fmt.Printf("We currently have %v of %v tickets available\n", remainingTickets, conferenceTickets)
-	fmt.Println("Get you tickets here to attend...")
+type guest = struct {
+	firstName    string
+	lastName     string
+	email        string
+	numOfTickets uint
+}
 
-	// infinite loop
+var guests []guest
+
+func getStrInput(prompt string) string {
+	// get str input from user
+	fmt.Print(prompt)
+	var userInput string
+	fmt.Scanln(&userInput)
+	return userInput
+}
+
+func getIntInput(prompt string) uint {
+	// get int input from user
+	fmt.Print(prompt)
+	var userInput uint
+	fmt.Scanln(&userInput)
+	return userInput
+}
+
+func welcomeMsg() {
+	fmt.Println("\n\nWelcome to Go Conference booking app.")
+	fmt.Printf("Book your tickets to attend. %v of %v tickets remaining...\n", availableTickets, totalTickets)
+}
+
+func isValidName(name string) (bool, string) {
+	return (len(name) > 2), "Entered name is too short"
+}
+
+func isValidEmail(email string) (bool, string) {
+	return (strings.Contains(email, "@")), "Invalid email address"
+}
+
+func validTickets(tickets uint) (bool, string) {
+	if tickets > availableTickets {
+		return false, fmt.Sprintf("Too many booked tickets. Only %v tickets remaining", availableTickets)
+	} else {
+		return true, ""
+	}
+}
+
+func getNValidateName(prompt string) string {
 	for {
-
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
-		// ask user for their name
-		fmt.Print("Enter your first name: ")
-		fmt.Scan(&firstName) // get user input and store it in pointer (&variable)
-
-		fmt.Print("Enter your last name: ")
-		fmt.Scan(&lastName)
-
-		fmt.Print("Enter your email address: ")
-		fmt.Scan(&email)
-
-		fmt.Print("Enter the number of tickets you want to purchase: ")
-		fmt.Scan(&userTickets)
-
-		// append returns the updated slice
-		bookings = append(bookings, firstName+" "+lastName)
-
-		firstNames := []string{} // := type inference
-
-		for _, booking := range bookings { // range returns an index (which we ignore), and item of slice at index
-			var names = strings.Fields(booking) // split string on space, returns a slice
-			firstNames = append(firstNames, names[0])
+		name := getStrInput(prompt)
+		name_valid, reason := isValidName(name)
+		if name_valid {
+			return name
+		} else {
+			fmt.Printf("%v, try again.\n", reason)
 		}
-		fmt.Println("This a list of those attending:", firstNames)
+	}
+}
 
-		remainingTickets = remainingTickets - userTickets
-		fmt.Println("Info:", firstName, lastName, email, userTickets)
-		fmt.Println(remainingTickets, "tickets remain")
+func getNValidateEmail(prompt string) string {
+	for {
+		email := getStrInput(prompt)
+		email_valid, reason := isValidEmail(email)
+		if email_valid {
+			return email
+		} else {
+			fmt.Printf("%v, try again.\n", reason)
+		}
+	}
+}
+
+func getNValidateTickets(prompt string) uint {
+	for {
+		ticketsBought := getIntInput(prompt)
+		valid_tickets, reason := validTickets(ticketsBought)
+		if valid_tickets {
+			return ticketsBought
+		} else {
+			fmt.Printf("%v, try again.\n", reason)
+		}
+	}
+}
+
+func main() {
+	// main function
+	for {
+		if availableTickets > 0 {
+			var guestUser guest
+			welcomeMsg() // print the welcome message and info
+
+			// get and validate name from user before returning it
+			first_name := getNValidateName("Enter your firstname: ")
+			guestUser.firstName = first_name
+
+			last_name := getNValidateName("Enter your lastname: ")
+			guestUser.lastName = last_name
+
+			// get email from user, validate and return it
+			email := getNValidateEmail("Enter your email address: ")
+			guestUser.email = email
+
+			// get and validate number of tickets user booked
+			ticketsBought := getNValidateTickets("Enter number of tickets to book: ")
+			guestUser.numOfTickets = ticketsBought
+
+			availableTickets -= ticketsBought // update count of available tickets
+			guests = append(guests, guestUser)
+			// display guests
+			fmt.Println(guests)
+		} else {
+			fmt.Println("Tickets sold out. Thank you.")
+			break
+		}
+
 	}
 }
